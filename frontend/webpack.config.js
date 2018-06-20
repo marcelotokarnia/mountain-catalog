bf4b5ce4341d2168d1cec9fd876857c24af41f06
Marcelo Tokarnia <marcelo.tokarnia@vtex.com.br>
2018-06-20 00:17:05 -0300

    deploy to www.trekkpedia.com new domain

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['es2015'],
+        },
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {

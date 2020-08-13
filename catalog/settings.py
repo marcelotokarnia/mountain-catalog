91aa10f38eb2fac4370d45eab5d14474fa10db6a
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-08-13 13:17:34 +0200

    try gdal heroku buildpack

--- catalog/settings.py
+++ catalog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
+GDAL_LIBRARY_PATH = '/usr/lib/libgdal.so'
 STRAVA_CLIENT_SECRET = os.getenv('STRAVA_CLIENT_SECRET', 'not_set')
 
 INSTALLED_APPS = [
